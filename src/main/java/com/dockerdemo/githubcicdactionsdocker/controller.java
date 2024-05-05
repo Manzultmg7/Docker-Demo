cca93e32ab739c64153bb5920deeeb2596d92cf4
Manjul Tamang <manzultamang777@gmail.com>
2024-05-05 12:28:20 +0545

    config

--- src/main/java/com/dockerdemo/githubcicdactionsdocker/controller.java
+++ src/main/java/com/dockerdemo/githubcicdactionsdocker/controller.java
@@ -1,14 +1,24 @@
 package com.dockerdemo.githubcicdactionsdocker;
 
 
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class controller {
 
-    @GetMapping("hello")
+    @GetMapping("/hello")
     public String hello() {
         return "hello, docker is working properly";
+    }
+
+    @GetMapping("/sayBye")
+    public  String bye(){
+        return "bye";
+    }
+    @GetMapping("/greet")
+    public String greet(){
+        return "Namaste";
     }
 }
